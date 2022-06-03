24d2cceddbb83f2cf5cafa3340abfc81a7b1c1b9
9mynamemj7 <ka5560@naver.com>
2022-06-03 20:01:50 +0900

    큐

---
+++ 자료구조/Code07-07.py
@@ -0,0 +1,80 @@
+## 함수
+def isQueueFull():
+    global SIZE, queue, front, rear
+    if (rear != SIZE-1):
+        return False
+    elif (rear == SIZE -1 and front == -1):
+        return True
+    else:
+        for i in range(front+1, SIZE, 1):
+            queue[i-1]  = queue[i]
+            queue[i] = None
+        front -= 1
+        rear -= 1
+        return False
+
+
+def enQueue(data):
+    global SIZE, queue, front, rear
+    if isQueueFull():
+        print('큐 꽉!')
+        return
+    rear += 1
+    queue[rear] = data
+
+def isQueueEmpty():
+    global SIZE, queue, front, rear
+    if front == rear:
+        return True
+    else:
+        return False
+
+def deQueue():
+    global SIZE, queue, front, rear
+    if (isQueueEmpty()):
+        print('큐 텅!')
+        return None
+    front += 1
+    data = queue[front]
+    queue[front] = None
+    return data
+
+def peek() :
+    global SIZE, queue, front, rear
+    if (isQueueEmpty()) :
+        print('큐 텅~~')
+        return None
+    return queue[front+1]
+
+
+## 전역
+SIZE = 5
+queue = [None for _ in range(SIZE)]
+front = rear = -1
+
+## 메인
+
+enQueue('화사')
+enQueue('솔라')
+enQueue('문별')
+enQueue('휘인')
+enQueue('선미')
+print(f'출구 --<--  {queue}  --<-- 입구')
+# enQueue('민중')
+# print(f'출구 --<--  {queue}  --<-- 입구')
+
+retData = deQueue()
+print('디큐 ==>', retData)
+retData = deQueue()
+print('디큐 ==>', retData)
+
+print(f'출구 --<--  {queue}  --<-- 입구')
+
+# print('다음 예정 손님 =>', peek())
+# print(f'출구 --<--  {queue}  --<-- 입구')
+
+enQueue('길동')
+print(f'출구 --<--  {queue}  --<-- 입구')
+
+enQueue('소희')
+print(f'출구 --<--  {queue}  --<-- 입구')
